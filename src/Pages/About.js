57b9a7a3d8cd6d960214af92fbc734219870d594
Nabeel M <nabeelm25698@gmail.com>
2022-10-28 20:09:16 +0530

    about_page_edited

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,6 +4,9 @@ const About = () => {
             <h1 className="text-6xl mb-4">
                 Github FInder
             </h1>
+            <p>It's a personal project in which user can search and view any GitHub profile with more focus on user's repos and each repo's watchers, forks, open issues and stars information.
+            Frontend is developed using React JS and styling is done by using tailwind CSS and DaisyUI.
+            </p>
         </div>
     )
 }
